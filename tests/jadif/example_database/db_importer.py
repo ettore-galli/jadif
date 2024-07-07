929fc2a0492956e5ea4cc81ee44a601ad55a23cf
ettore-galli <ettore.galli@gmail.com>
2024-07-07 14:21:54 +0200

    Use Inject

--- tests/jadif/example_database/db_importer.py
+++ tests/jadif/example_database/db_importer.py
@@ -3,6 +3,10 @@ from jadif.jadif import dependency
 from tests.jadif.example_database.repo import BaseDBRepo
 
 
+def import_into_db_injectable(repo: BaseDBRepo) -> list[str]:
+    return repo.perform()
+
+
 def import_into_db() -> list[str]:
     repo = dependency.resolve(BaseDBRepo)
     return repo.perform()
--- tests/jadif/test_dependency_injection_database.py
+++ tests/jadif/test_dependency_injection_database.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 
-from jadif.jadif import dependency
+from jadif.jadif import Injected, dependency
 
-from tests.jadif.example_database.db_importer import import_into_db
+from tests.jadif.example_database.db_importer import (
+    import_into_db,
+    import_into_db_injectable,
+)
 from tests.jadif.example_database.repo import BaseDBRepo, DummyRepo, SQLiteDbRepo
 from tests.jadif.example_reader.base import InputFile
 from tests.jadif.example_reader.read_file_service import (
@@ -44,3 +47,14 @@ def test_dependency_injection_process_dummy() -> None:
     make_dummy_dependency(source_file=Path("dummy.txt"))
 
     assert import_into_db() == ["dummy", "data", "any"]
+
+
+def test_dependency_injection_via_injected() -> None:
+    make_sqlite_dependency(source_file=EXAMPLE_FILE)
+
+    import_db = Injected(import_into_db_injectable)
+
+    assert import_db() == [
+        "Example content in file\n",
+        "This is the content of the actual file",
+    ]
