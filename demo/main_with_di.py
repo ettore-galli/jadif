42372ce1ae6ac7f059b22c469f898accd9472087
ettore-galli <ettore.galli@gmail.com>
2024-07-20 09:47:48 +0200

    Improve mains

--- demo/main_with_di.py
+++ demo/main_with_di.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 
 from demo.importer_with_di.clock import get_current_datetime
-from demo.importer_with_di.configuration import get_configuration
+from demo.importer_with_di.configuration import Configuration, get_configuration
 from demo.importer_with_di.data_repo import DataRepo, get_connection
 from demo.importer_with_di.read_file_service import FileReader
 
@@ -11,8 +11,7 @@ def get_input_file() -> Path:
     return Path(sys.argv[1])
 
 
-if __name__ == "__main__":
-    configuration = get_configuration(config_file=get_input_file())
+def import_file(configuration: Configuration) -> None:
 
     repo = DataRepo(connection=get_connection(configuration=configuration))
 
@@ -21,4 +20,15 @@ if __name__ == "__main__":
     for line in reader.read_file(Path(configuration.input_file)):
         repo.insert(content=line, import_date=get_current_datetime())
 
+
+def show_database_content(configuration: Configuration) -> None:
+    repo = DataRepo(connection=get_connection(configuration=configuration))
+
     print(list(repo.get_all_data()))  # noqa: T201
+
+
+if __name__ == "__main__":
+    configuration = get_configuration(config_file=get_input_file())
+
+    import_file(configuration=configuration)
+    show_database_content(configuration=configuration)
